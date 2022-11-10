4b4b70aec0dbf64cfc93671eacd7131920071c7e
Gabriel Souza <gbsouza.dev@gmail.com>
2022-11-10 19:58:52 -0300

    contact form fix

--- js/main.js
+++ js/main.js
@@ -2,3 +2,8 @@ const year = document.getElementById("year")
 const thisYear = new Date().getFullYear()
 year.setAttribute("datetime", thisYear)
 year.textContent = thisYear
+
+function formBehavior(){
+    document.getElementById('contactForm').style.display = 'none';
+    document.getElementById('contactH2').innerHTML = 'Your message has been successfully sent!';
+}
